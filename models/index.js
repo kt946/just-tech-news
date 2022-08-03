92055ceceeb0b9e810b1c7fce1405c79bc6b0655
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-08-03 19:33:49 -0400

    fixed associations

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ User.hasMany(Post, {
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+    onDelete: 'SET NULL'
 });
 
 User.belongsToMany(Post, {
@@ -18,24 +18,24 @@ User.belongsToMany(Post, {
     as: 'voted_posts',
 
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+    onDelete: 'SET NULL'
 });
 
 Post.belongsToMany(User, {
     through: Vote,
     as: 'voted_posts',
     foreignKey: 'post_id',
-    onDelete: 'CASCADE'
+    onDelete: 'SET NULL'
 });
 
 Vote.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+    onDelete: 'SET NULL'
 });
 
 Vote.belongsTo(Post, {
     foreignKey: 'post_id',
-    onDelete: 'CASCADE'
+    onDelete: 'SET NULL'
 });
 
 User.hasMany(Vote, {
@@ -48,17 +48,17 @@ Post.hasMany(Vote, {
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+    onDelete: 'SET NULL'
 });
 
 Comment.belongsTo(Post, {
     foreignKey: 'post_id',
-    onDelete: 'CASCADE'
+    onDelete: 'SET NULL'
 });
 
 User.hasMany(Comment, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+    onDelete: 'SET NULL'
 });
 
 Post.hasMany(Comment, {
